0f2e8dc11062f0e51729b70ce2885c030c89cf07
Kenzothd <Kenzothd@hotmail.com>
2022-09-12 10:56:55 +0800

    update

---
+++ server.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+const express = require("express");
+
+//config
+const app = express();
+const MONGO_URI = process.env.MONGO_URI ?? "mongodb://localhost:27017/test";
+const PORT = process.env.PORT ?? 3300;
+mongoose.connect(MONGO_URI, {}, () => {
+  console.log("the connection with mongodb is established");
+}); //? auto create test collection
+
+//middleware
+app.use(morgan("dev"));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("yay");
+});
+
+app.listen(PORT, () => {
+  console.log(`express started on ${PORT}`);
+});
